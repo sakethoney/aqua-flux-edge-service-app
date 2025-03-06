d10a43bbe31b8ee816abcf53743826cb0aa4b58a
sakethoney <saket.kumar.cs@gmail.com>
2025-03-06 00:48:32 -0500

    added graphQL provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { GraphQLProviders } from './graphql.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ 
+    eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay(),
+    ...GraphQLProviders)]
 };
---
+++ src/app/graphql.config.ts
@@ -0,0 +1,24 @@
+import { provide } from '@angular/core';
+import { HttpLink } from 'apollo-angular/http';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
+
+// The Spring Boot GraphQL endpoint:
+const uri = 'http://localhost:8080/graphql';
+
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const http = httpLink.create({ uri });
+  // If needed, you can add an ApolloLink array for auth, logging, etc.
+  return {
+    link: ApolloLink.from([http]),
+    cache: new InMemoryCache(),
+  };
+}
+
+// This array can be spread into the `providers: []` array in your bootstrap.
+export const GraphQLProviders = [
+  provide(APOLLO_OPTIONS, {
+    useFactory: createApollo,
+    deps: [HttpLink],
+  }),
+];
